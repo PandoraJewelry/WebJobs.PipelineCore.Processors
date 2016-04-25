38cf45aed151b3046460e796dd48307afb3e24db
mark newman <mark.newman@pandora.net>
2016-04-25 08:43:46 -0400

    inital checkin

---
+++ Pandora.Azure.WebJobs.PipelineCore.Processors/HalfLifeRenewMessageProcessor.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) PandoraJewelry. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Pandora.ServiceBus;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pandora.Azure.WebJobs.PipelineCore.Processors
+{
+    public class HalfLifeRenewMessageProcessor : IMessageProcessor
+    {
+        public async Task Invoke(IPipelineContext context, Func<Task> next, CancellationToken cancellationToken)
+        {
+            var done = context.Message.AutoRenew();
+            try
+            {
+                await next();
+            }
+            finally
+            {
+                done();
+            }
+        }
+    }
+}
---
+++ Pandora.Azure.WebJobs.PipelineCore.Processors/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Pandora.Azure.WebJobs.PipelineCore.Processors")]
+[assembly: AssemblyDescription("Implements a variety of processors")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("PandoraJewelry")]
+[assembly: AssemblyProduct("Pandora.Azure.WebJobs.PipelineCore.Processors")]
+[assembly: AssemblyCopyright("Copyright © Pandora 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("75eb139d-ed85-41ff-9562-c7eb56640631")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("Pandora.Azure.WebJobs.PipelineCore.Processors.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
